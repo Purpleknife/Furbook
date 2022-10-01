dabec908c32d9c2484b0f737ab66d0333ca84da3
sylvielcq <slcq.dev@gmail.com>
2022-10-01 14:11:31 -0700

    style friend cards

--- front-end/src/components/Friend.js
+++ front-end/src/components/Friend.js
@@ -1,11 +1,20 @@
 import React from "react";
-// import './FriendItem.scss';
+import './Friend.scss';
 
 const Friend = (props) => {
   return (
-    <li>
-      <p>{props.friend_id}</p>
-    </li>
+      <main className="friend-card">
+        <div className="friend-info">
+          <img className="friend-picture"
+            src={props.picture}
+          />
+          <p>{props.first_name} {props.last_name}</p>
+        </div>
+        <div className="friend-actions">
+          <button class="btn"><i class="fa-solid fa-messages"></i> Message</button>
+          <button class="btn"><i class="fa-solid fa-xmark"></i> Remove</button>
+        </div>
+      </main>
   );
 };
 
--- front-end/src/components/Friendships.js
+++ front-end/src/components/Friendships.js
@@ -1,17 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Friend from './Friend';
-// import './Friendships.scss';
+import './Friendships.scss';
 
 const Friendships = () => {
 
   // INITIAL STATE
   const [state, setState] = useState({
-    friends: {},
-    pending: {}
+    friends: [],
+    pending: []
   });
-
-  // const [friends, setFriends] = useState([]);
 
   // API CALLS
   useEffect(() => {
@@ -20,39 +18,28 @@ const Friendships = () => {
       axios.get('/friendships/pending')
     ]).then((all) => {
       setState(prev => ({
+        ...prev,
         friends: all[0].data,
         pending: all[1].data
       }));
     });
   }, [])
 
-
-
-  // const getFriends = () => {
-  //   axios.get('/friendships')
-  //     .then((res) => {
-  //       console.log("Friends data from Axios request: ", res.data);
-  //       setFriends(res.data);
-  //     })
-  // };
-
-  // useEffect(() => {
-  //   getFriends();
-  // }, []);
-
-  const friendItem = friends.map(friend => {
+  // INDIVIDUAL FRIEND COMPONENT
+  const friendItem = state.friends.map(friend => {
     return (
       <Friend
         key={friend.id}
-        friend_id={friend.receiver}
+        first_name={friend.first_name}
+        last_name={friend.last_name}
+        picture={friend.image_url}
       />
     )
   });
 
   return (
-    <main>
-      <div className="test">FRIENDSHIPS</div>
-      <div>{friendItem}</div>
+    <main className='friends-list'>
+      {friendItem}
     </main>
   );
 };
