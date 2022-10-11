60fc910a9f5743f94b1ceab80907394db234e32d
sylvielcq <slcq.dev@gmail.com>
2022-10-11 11:52:31 -0700

    DB updates

--- back-end/db/seeds/03_posts.sql
+++ back-end/db/seeds/03_posts.sql
@@ -1,11 +1,13 @@
 INSERT INTO posts (creator, content, image_url, date_posted)
 VALUES
 (4, 'Me and my soulmate', 'https://i.pinimg.com/736x/bb/b3/dc/bbb3dc9fff862926e282aa66921d2297--allposters-com-mini-pigs.jpg', '2022-09-22'),
-(3, 'Spending time at my favorite bookstore.', 'https://images.unsplash.com/photo-1535930749574-1399327ce78f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8bGFicmFkb3IlMjBib29rfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60', '2022-10-01'),
-(4, 'Don''t bacon my heart 💔', 'https://www.rd.com/wp-content/uploads/2021/03/GettyImages-1273972629.jpg', '2022-08-15'),
+(5, 'My human loves me so much that they got a statue of me', 'https://dachshundbonus.com/wp-content/uploads/2019/09/72125582_2521062127988015_6511969465041682432_n.jpg', '2022-06-25'),
 (1, 'Daily fitness class with my friends.', 'https://media.istockphoto.com/photos/funny-cats-are-doing-exercise-with-stability-ball-fitness-club-picture-id836142768?k=20&m=836142768&s=612x612&w=0&h=-bg7Kq2MZ5wV3U9PUX-yHkbAoZCYD9HflGqf51men5A=', '2022-08-10'),
+(4, 'Don''t go bacon my heart 💔', 'https://www.rd.com/wp-content/uploads/2021/03/GettyImages-1273972629.jpg', '2022-08-15'),
 (6, '❤️', 'https://images.unsplash.com/photo-1526336179256-1347bdb255ee?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=688&q=80', '2022-10-02'),
 (1, 'My fitness classes are paying off.', 'https://i.redd.it/zwynel51x3e11.jpg', '2022-09-30'),
+(5, 'When you open the camera and it''s on selfie mode', 'https://i.pinimg.com/originals/f3/3a/0c/f33a0c012c146713d4061f503f4779ba.jpg', '2022-07-01'),
+(3, 'Spending time at my favorite bookstore.', 'https://images.unsplash.com/photo-1535930749574-1399327ce78f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8bGFicmFkb3IlMjBib29rfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60', '2022-10-01'),
 (4, 'Cuddle time with my human :)', 'https://i.pinimg.com/564x/a4/29/b6/a429b64202365ce04430290f7f3d178a.jpg', '2022-05-12'),
 (5, 'Hangin with my buuuddy', 'https://i.pinimg.com/originals/c7/d3/f0/c7d3f070f5e486b4b8d269963642d3a8.jpg', '2022-07-02'),
 (2, 'I wish all the best to my successor.', 'https://www.cleveland.com/resizer/yQslUKD32vw2jfKWJ-0Hf5rRnIQ=/800x0/smart/cloudfront-us-east-1.images.arcpublishing.com/advancelocal/6LHQLBXCG5HKZLRBPH5JJS2EAI.jpg', '2022-07-02');
--- back-end/db/seeds/05_comments.sql
+++ back-end/db/seeds/05_comments.sql
@@ -1,14 +1,15 @@
 INSERT INTO comments (post_id, user_id, content, date_added)
 VALUES
-(8, 2, 'Pawesome!', '2022-07-02'),
-(8, 4, 'Where was this taken?', '2022-07-03'),
-(7, 6, 'You were really cute!', '2022-05-12'),
-(7, 1, 'Me and my human do the same ❤️', '2022-05-13'),
-(7, 3, '🥰🥰🥰🥰🥰🥰🥰🥰🥰', '2022-05-13'),
-(6, 6, 'Looking good!🙌', '2022-10-01'),
-(4, 6, 'Which club is this?', '2022-08-10'),
-(4, 1, 'It was at Vanity Fur!!', '2022-08-10'),
-(4, 5, 'Can I join next time?', '2022-08-11'),
 (1, 1, 'You make a cute couple 😊', '2022-09-23'),
-(2, 2, 'Can''t wait to read your new book Mrs. Growling.', '2022-10-02'),
-(3, 5, 'I couldn''t if I fried.', '2022-08-15');
+(2, 3, 'Hoomans are really obssessed with us 😅', '2022-07-13'),
+(3, 6, 'Which club is this?', '2022-08-10'),
+(3, 1, 'It was at Vanity Fur!!', '2022-08-10'),
+(3, 5, 'Can I join next time?', '2022-08-11'),
+(4, 5, 'I couldn''t if I fried.', '2022-08-15'),
+(6, 6, 'Looking good!🙌', '2022-10-01'),
+(8, 2, 'Can''t wait to read your new book Mrs. Growling.', '2022-10-02'),
+(9, 6, 'You were really cute!', '2022-05-12'),
+(9, 1, 'Me and my human do the same ❤️', '2022-05-13'),
+(9, 3, '🥰🥰🥰🥰🥰🥰🥰🥰🥰', '2022-05-13'),
+(10, 2, 'Pawesome!', '2022-07-02'),
+(10, 4, 'Where was this taken?', '2022-07-03');
